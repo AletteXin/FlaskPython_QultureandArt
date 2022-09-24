04cd8bbc99ceaae15a23e13f3c1e300286cdb246
AletteXin <85789376+AletteXin@users.noreply.github.com>
2022-09-24 17:39:24 -0400

    Update views.py

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -21,7 +21,7 @@ users_blueprint = Blueprint('users',
 @users_blueprint.route('/new', methods=['GET'])
 def new():
 #     query = User.delete().where(User.username == 'stephaniechiu')
-    query_ = User.delete_by_id(9)
+    query_ = User.delete_by_id(7)
     query_.execute()
     return render_template('/users/new.html')
 
