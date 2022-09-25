9610e63ec2ff10d47fe3f9c76e86e8df3f8dbe85
AletteXin <85789376+AletteXin@users.noreply.github.com>
2022-09-25 19:44:58 -0400

    Update database.py

--- database.py
+++ database.py
@@ -31,11 +31,11 @@ def return_db():
             db_config['database'],
             max_connections=os.getenv('DB_POOL', 5),
             stale_timeout=os.getenv('DB_TIMEOUT', 300),  # 5 minutes.
-            database = os.getenv('RDS_READS_DB_NAME')
-            user = os.getenv('RDS_USER')
-            password = os.getenv('RDS_DB_PASS')
-            host = os.getenv('RDS_HOST')
-            port = os.getenv('RDS_DB_PORT')
+            database = os.getenv('RDS_READS_DB_NAME'),
+            user = os.getenv('RDS_USER'),
+            password = os.getenv('RDS_DB_PASS'),
+            host = os.getenv('RDS_HOST'),
+            port = os.getenv('RDS_DB_PORT'))
 #             user=db_config.get('user', None),
 #             password=db_config.get('password', None),
 #             host=db_config.get('host', 'localhost'),
