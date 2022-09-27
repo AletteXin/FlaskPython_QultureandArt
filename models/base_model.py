d3b39fcd076e0747d387ed3283ef79eb0018f5b8
AletteXin <85789376+AletteXin@users.noreply.github.com>
2022-09-26 23:49:49 -0400

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -15,7 +15,8 @@ class BaseModel(pw.Model):
 
         if len(self.errors) == 0:
             self.updated_at = datetime.datetime.now()
-            return super(BaseModel, self).save(*args, **kwargs)
+            return 1
+#             return super(BaseModel, self).save(*args, **kwargs)
         else:
             for list in self.errors:
                 flash(list)
