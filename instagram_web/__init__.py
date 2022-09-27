702665d5f15335322881b94fbaf01a3deb18bd14
AletteXin <85789376+AletteXin@users.noreply.github.com>
2022-09-26 22:29:40 -0400

    Update __init__.py

--- instagram_web/__init__.py
+++ instagram_web/__init__.py
@@ -57,7 +57,8 @@ def home():
         user_liked = []
 
     users = User.select()
-    images = Image.select().order_by(Image.date_posted.desc())
+#     images = Image.select().order_by(Image.date_posted.desc())
+    images = Image.select().order_by(Image.id.desc())
     users_with_images = prefetch(images, users)
 
 
@@ -65,21 +66,16 @@ def home():
 
 @app.route("/about")
 def about():
-    if session.get('user_id'):
-        user = User.get_or_none(User.id == session["user_id"])
-        if user:
-            idols = User.select().join(Follow, on = Follow.idol_id == User.id).where(Follow.follower_id == user.id, Follow.approved == "1")
-            user_liked = Image.select().join(Likes, on = Likes.liker_id == user.id).where(Likes.image_id == Image.id)
+#     if session.get('user_id'):
+#         user = User.get_or_none(User.id == session["user_id"])
+#         if user:
+#             idols = User.select().join(Follow, on = Follow.idol_id == User.id).where(Follow.follower_id == user.id, Follow.approved == "1")
+#             user_liked = Image.select().join(Likes, on = Likes.liker_id == user.id).where(Likes.image_id == Image.id)
 
-    else:
-        user = None 
-        idols = []
-        user_liked = []
-
-    users = User.select()
-#     images = Image.select().order_by(Image.date_posted.desc())
-    images = Image.select().order_by(Image.id.desc())
-    users_with_images = prefetch(images, users)
+#     else:
+#         user = None 
+#         idols = []
+#         user_liked = []
 
 
     return render_template('about.html', user = user, users_with_images = users_with_images, idols = idols, user_liked = user_liked)
